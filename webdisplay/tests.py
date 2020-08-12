e3c13cfe1b80eabce83398b31248c47386bdfefd
anurag5398 <anurags2898@gmail.com>
2020-08-13 02:30:00 +0530

    testfile

--- webdisplay/tests.py
+++ webdisplay/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+def test_upload_video(self):
+    video = SimpleUploadedFile("file.mp4", "file_content", content_type="video/mp4")
+    self.client.post(reverse('app:some_view'), {'video': video})
